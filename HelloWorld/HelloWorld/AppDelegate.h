aa042563da26ff8f70942f19e38804f74ac0e425
wxlei <1102259356@qq.com>
2015-12-30 14:20:04 +0800

    helloworld

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Bizideal on 15/12/30.
+//  Copyright © 2015年 wxlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
